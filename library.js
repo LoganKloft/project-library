f71bd7a77f6ea5fcb1fb482d0560626bd40ffdfb
Logan Kloft <lkloft@outlook.com>
2023-08-21 09:12:27 -0700

    Fix creation of book to occur on form submit event as opposed to the click event of the submit button on the form.

--- library.js
+++ library.js
@@ -17,7 +17,7 @@ class Library {
         this.myLibrary.push(bk);
         this.drawLibrary();
     }
-    
+
     removeBookFromLibrary(index) {
         this.myLibrary.splice(index, 1);
         this.drawLibrary();
@@ -25,39 +25,38 @@ class Library {
 
     drawLibrary() {
         const container = document.querySelector('.container');
-    
+
         // remove previous books
-        while(container.firstChild) {
+        while (container.firstChild) {
             container.removeChild(container.firstChild)
         }
-    
+
         // draw all books
-        for(let i = 0; i < this.myLibrary.length; i++)
-        {
+        for (let i = 0; i < this.myLibrary.length; i++) {
             let bk = this.myLibrary[i];
-    
+
             // create book container
             const book = document.createElement('div');
             book.classList.add('book');
-    
+
             // add title element to book container
             const title = document.createElement('p');
             title.classList.add('title');
             title.innerText = `Title: ${bk.title}`;
             book.appendChild(title);
-    
+
             // add author element to book container
             const author = document.createElement('p');
             author.classList.add('author');
             author.innerText = `Author: ${bk.author}`;
             book.appendChild(author);
-    
+
             // add pages element to book container
             const pages = document.createElement('p');
             pages.classList.add('pages');
             pages.innerText = `Pages: ${bk.pages}`;
             book.appendChild(pages);
-    
+
             // add whether or not the book has been read, which is just a checkbox
             // the user may check and uncheck.
             const div = document.createElement('div');
@@ -68,21 +67,20 @@ class Library {
             div.appendChild(read_text);
             const read = document.createElement('input')
             read.setAttribute('type', 'checkbox');
-            if (bk.read)
-            {
+            if (bk.read) {
                 read.checked = true;
             }
             read.setAttribute('data-index', i.toString());
             read.addEventListener('click', (e) => this.readBookHandler(e));
             div.appendChild(read);
-    
+
             // button for deleting the book
             const delete_button = document.createElement('button');
             delete_button.innerText = `delete`;
             delete_button.setAttribute('data-index', i.toString());
             delete_button.addEventListener('click', (e) => this.deleteBookHandler(e));
             book.appendChild(delete_button);
-    
+
             container.appendChild(book);
         }
     }
@@ -91,7 +89,7 @@ class Library {
         let index = Number(event.target.getAttribute('data-index'));
         this.removeBookFromLibrary(index);
     }
-    
+
     readBookHandler(event) {
         let index = Number(event.target.getAttribute('data-index'));
         this.myLibrary[index].read = !this.myLibrary[index].read;
@@ -102,12 +100,11 @@ var library = new Library();
 
 // create a random book
 let titles = ['To Kill a Mockingbird', '1984', 'The Lord of the Rings', 'The Catcher in the Rye',
-'The Great Gatsby', 'The Lion, the Witch and the Wardrobe', 'Lord of the Flies', 'Animal Farm',
-'Catch-22', 'The Grapes of Wrath', 'Gone with the Wind', 'Slaughterhouse-Five']
+    'The Great Gatsby', 'The Lion, the Witch and the Wardrobe', 'Lord of the Flies', 'Animal Farm',
+    'Catch-22', 'The Grapes of Wrath', 'Gone with the Wind', 'Slaughterhouse-Five']
 let authors = ['Harper Lee', 'George Orwell', 'J.R.R Tolkien', 'J.D. Salinger', 'F. Scott Fitzgerald', 'C.S. Lewis', 'William Golding', 'George Orwell', 'Joseph Heller', 'John Steinbeck', 'Margaret Mitchell']
 function createBooks(x = 1) {
-    while(x > 0)
-    {
+    while (x > 0) {
         let index = Math.floor(Math.random() * titles.length);
         let read = index % 2 == 0 ? true : false;
         let pages = Math.floor(Math.random() * 999) + 1; // 0 - 999
@@ -119,8 +116,8 @@ function createBooks(x = 1) {
 
 // get data from form when submitted. Then clear the form
 // and create a new book entry. 
-const submit = document.querySelector('#submit');
-submit.addEventListener('click', (event) => {
+const form = document.querySelector('#form');
+form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     // get form data
